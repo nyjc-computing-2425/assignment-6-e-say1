4ef1fa49bffa905b59de2e5fa14773dec952f0ff
e-say1 <120434420+e-say1@users.noreply.github.com>
2024-03-31 07:57:35 +0000

    assignment 6 final submission

--- main.py
+++ main.py
@@ -1,20 +1,76 @@
 from datetime import datetime
-import time
+from time import sleep
 
 
 # Part 1
-def clock(n):
+def clock(n: int) -> str:
+    """
+    returns the current time for n seconds
+
+    Parameters
+    ----------
+    n: int
+        number of seconds the function will run for
+    """
     # Your code here
-    pass
+    for i in range(n):
+        x = datetime.now()
+        time = x.strftime("%H:%M:%S")
+        print(f"{time}", end="\r")
+        sleep(1)
+
 
 
 # Part 2
-def lcm(a, b):
+def lcm(a: int, b: int) -> int:
+    """
+    returns the lowest common multiple of a and b
+
+    Parameters
+    ----------
+    a: int
+        first number
+    b: int:
+        second number
+    """
     # Your code here
-    pass
+    done = False
+    a_og = a
+    b_og = b
+    while done is False:
+        if a == b:
+            lcm = a
+            done = True
+        elif a > b:
+            b += b_og
+        elif b > a:
+            a += a_og
+    return lcm
 
 
 # Part 3
-def gcf(a, b):
-    # Your code here
-    pass
+def gcf(a: int, b: int) -> int:
+    """
+    returns the greatest common factor of a and b
+
+    Parameters
+    ----------
+    a: int
+    first number
+
+    b: int
+    second number
+    """
+    # Your code here\
+    done = False
+    while done is False:
+        if b == 0:
+            gcf = a
+            done = True
+        else:
+            remainder = a % b
+            a = b
+            b = remainder
+    return gcf
+
+
